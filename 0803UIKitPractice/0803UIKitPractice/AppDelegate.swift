ffc60ad570deb712bbc17ee13fdbac69dfa577a6
Tsai-Ting Yang <u9934007@gmail.com>
2017-08-03 09:09:27 +0800

    init

---
+++ 0803UIKitPractice/0803UIKitPractice/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  0803UIKitPractice
+//
+//  Created by 楊采庭 on 2017/8/3.
+//  Copyright © 2017年 楊采庭. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = ViewController()
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
+        
+        return true
+
+     
+    }
+}
+
---
+++ 0803UIKitPractice/0803UIKitPractice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  0803UIKitPractice
+//
+//  Created by 楊采庭 on 2017/8/3.
+//  Copyright © 2017年 楊采庭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+    }
+
+    
+
+}
+
